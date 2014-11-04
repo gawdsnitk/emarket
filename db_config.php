fcbaca90b622abdd0b80e7c96b54ddce340042da
Harsh <shry.harsh@gmail.com>
2014-11-04 15:50:33 +0530

    first and final commit

---
+++ db_config.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * All database connection variables
+ */
+
+        define('DB_USER', "gawdsin_gawdsin"); // db user
+define('DB_PASSWORD', "KckZ6ZJ?38wm"); // db password (mention your db password here)
+define('DB_DATABASE', "gawdsin_emarket"); // database name
+define('DB_SERVER', "localhost"); // db server*/
+?>
